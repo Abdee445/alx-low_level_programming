13130191ae93853ce35da25c7858494f0cc87afd
abdee445 <abdallahmusa300@yahoo.com>
2022-09-08 18:08:26 -0700

    lol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 # include <stdio.h>
-/*main function*/
-int main (void)
+/*
+ *main function is the major function and should always return 0
+*/
+int main(void)
 {
 	char Mystr[] = "Programming is like building a multilingual puzzle";
+	
 	puts(Mystr);
 	return (0);
 }
