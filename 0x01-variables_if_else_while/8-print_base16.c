26e49ff3dad8de021e92cb11900350de8de787cd
abdee445 <abdallahmusa300@yahoo.com>
2022-09-09 20:45:44 -0700

    lol

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numbers = '0';
+	char alphabets = 'a';
+
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	while (alphabets <= 'f')
+	{
+		putchar(alphabets);
+	}
+	putchar('\n');
+	return(0);
+}
