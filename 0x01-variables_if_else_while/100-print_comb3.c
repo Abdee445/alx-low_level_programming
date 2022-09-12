d1f2af0c89166527763e82f6b8082920af9cab0e
abdee445 <abdallahmusa300@yahoo.com>
2022-09-12 12:45:14 -0700

    lol

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - Entry point
+ *
  *
  * Return: Always 0.
  */
