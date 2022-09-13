eafb129c30d01ac1b6990fabf60176f9408f7cf8
abdee445 <abdallahmusa300@yahoo.com>
2022-09-12 21:56:01 -0700

    lol

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens, units;
+
+	for (tens = 0; tens <= 98; tens++)	
+	{
+		for (units = tens + 1; units <= 99; units++)	
+		{
+			putchar((tens / 10) + '0');
+			putchar((tens % 10) + '0');
+			putchar(' ');
+			putchar((units / 10) + '0');
+			putchar((units % 10) + '0');
+			if (tens == 98 && units == 99)
+				continue;
+			putchar(',');
+			putchar(' ');	
+		}	
+	}
+	putchar('\n');
+	return (0);	
+}
