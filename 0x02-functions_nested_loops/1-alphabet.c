efad8383408455dd827ba47cafe57cee0aecd61d
abdee445 <abdallahmusa300@yahoo.com>
2022-09-13 14:35:51 -0700

    lol

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)	
+{	
+	char alphabet = 'a';
+
+        while (alphabet <= 'z')
+        {
+		_putchar(alphabet);
+		alphabet++;
+        }
+        {
+		_putchar('\n');
+        }
+}
