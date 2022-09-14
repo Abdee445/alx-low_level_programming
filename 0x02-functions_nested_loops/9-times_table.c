a505d9b954b495f748481bcc1d5bfd77bcc65917
abdee445 <abdallahmusa300@yahoo.com>
2022-09-14 15:35:49 -0700

    lol

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+	for (i = 0; i <= 9; i++)
+	{
+
+		
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			k = i * j;
+			if (i <= 9)
+			_putchar(',');
+			if ((k <= 9) && (j > 0))
+			{_putchar(' ');
+				_putchar(' ');}
+			else{
+				_putchar(' ');
+				_putchar((k / 10) + '0');}
+				_putchar((k % 10) + '0');
+		
+		}
+		_putchar('\n');
+	}
+}
