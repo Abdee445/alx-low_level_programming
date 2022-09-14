84f152dc2c268210953363ac8e02840824fa9722
abdee445 <abdallahmusa300@yahoo.com>
2022-09-13 18:57:25 -0700

    lol

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of an int
+ *
+ * @p: number to be checked
+ * Return: p if int s positive or zero, - if int is negative,
+ */
+int _abs(int p)
+{
+	if (p >= 0)
+		return (p);
+	else
+		return (-p);
+}
