a29cba5a52ad7304a01c544d867bd9cfae3ca4dd
abdee445 <abdallahmusa300@yahoo.com>
2022-09-13 19:36:32 -0700

    lol

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * units - print the last digit
+ *
+ * @num: number to bw passed
+ * _putchar: send item to standard outout
+ * Return: p if int s positive or zero, - if int is negative,
+ */
+int print_last_digit(int num);
+{
+	int units;
+
+	units = (num % 10);
+
+	if (units < 0)
+	{
+		units = (-1 * units);
+	}
+	_putchar(units + '0');
+	return (last);
+}
