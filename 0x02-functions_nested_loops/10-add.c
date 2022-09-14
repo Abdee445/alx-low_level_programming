9ace4decec69c672f5a489d22070e93189538730
abdee445 <abdallahmusa300@yahoo.com>
2022-09-14 16:13:58 -0700

    lol

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - function that adds two integers
+ *_putchar - function that sends items to output
+ */
+int add(int a, int b)
+{
+	a = 10;
+	b = 15;
+
+	_putchar(a + b);
+	return(0);
+}
