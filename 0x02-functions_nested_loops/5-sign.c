1fe26b40f0792c77219b8b18856b03a5ebcb4105
abdee445 <abdallahmusa300@yahoo.com>
2022-09-13 18:38:25 -0700

    lol

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - print the sign of an int
  *
  * _putchar - send value to standard output
- *
+ * Ap: number to be checked
  * Return: + if int s positive, - if int is negative, and 0 if int is 0
  */
 int print_sign(int p)
@@ -11,16 +11,16 @@ int print_sign(int p)
 	if (p > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
-	else if (p ==0)
+	else if (p == 0)
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
