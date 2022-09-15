fbda5cc3073b7c4710b0b8f702a2ec6237375a2d
abdee445 <abdallahmusa300@yahoo.com>
2022-09-15 06:46:18 -0700

    lol

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative()
+{
+	
+}
