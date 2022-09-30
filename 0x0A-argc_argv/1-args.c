bca756ffdc60359e7540eb5a6fe113b6fe566d8f
abdee445 <abdallahmusa300@yahoo.com>
2022-09-30 14:17:25 -0700

    lol

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: counts no of arguments
+ * @argv: stores arguments in an array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
